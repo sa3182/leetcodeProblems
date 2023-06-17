20124636d872ec8e5b7ca71e1a375ba48a0c7059
Shravani Athkuri <sa3182@rit.edu>
2023-06-17 17:39:37 -0400

    added twoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -3,16 +3,15 @@ import java.util.Map;
 
 public class TwoSum {
 
-        public int[] twoSum(int[] nums, int target) {
-
-            Map<Integer, Integer> twoSum = new HashMap<>();
-            for(int i=0;i<nums.length;i++){
-                int temp = target-nums[i];
-                if(twoSum.containsKey(temp)){
-                    return new int[]{twoSum.get(temp), i};
-                }
-                twoSum.put(nums[i],i);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> twoSum = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp = target-nums[i];
+            if(twoSum.containsKey(temp)){
+                return new int[]{twoSum.get(temp), i};
             }
-            return new int[]{};
+            twoSum.put(nums[i],i);
         }
+        return new int[]{};
+    }
 }
