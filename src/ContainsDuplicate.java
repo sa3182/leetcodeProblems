c1ec179c77159e868a3c3ce6394923e8fbde8c5e
Shravani Athkuri <sa3182@rit.edu>
2023-06-17 17:27:56 -0400

    added containsDuplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> dup = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(dup.contains(nums[i])){
+                return true;
+            }
+            dup.add(nums[i]);
+        }
+        return false;
+    }
+}
