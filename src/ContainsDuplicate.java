ed2e65ccf6abfc6d0843712e89e9770e5e6e4479
Shravani Athkuri <sa3182@rit.edu>
2023-06-17 17:38:28 -0400

    added twoSum

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate (int[] nums) {
         HashSet<Integer> dup = new HashSet<>();
         for(int i=0;i<nums.length;i++){
             if(dup.contains(nums[i])){
