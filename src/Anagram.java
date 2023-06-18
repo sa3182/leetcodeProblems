60cade63078e1e26fe0b386da5b6a8151d062168
Shravani Athkuri <sa3182@rit.edu>
2023-06-18 14:35:00 -0400

    updated isAnagram

--- src/Anagram.java
+++ src/Anagram.java
@@ -9,14 +9,11 @@ public class Anagram {
         int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a'] += 1;
+            freq[t.charAt(i)-'a'] -= 1;
         }
-        for(int i=0;i<t.length();i++){
-            if( freq[t.charAt(i)-'a']==0){
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0)
                 return false;
-            }
-            else {
-                freq[t.charAt(i)-'a'] -= 1;
-            }
         }
         return true;
     }
